3e02fd6bf351539a12a5773f035a98ecf15ea8bd
Its-a-Me-2345 <arnavvivek13@gmail.com>
2025-11-13 00:08:17 +0530

    Bidirectional BFS for Shortest Path between 2 users and tweaked the visualizer to show shortesr path

--- backend/server.js
+++ backend/server.js
@@ -12,30 +12,32 @@ app.use(cors());
 app.use(express.json());
 //mongoose.connect promise return karta hai. 
 //toh ek async function bana diye aur jab tak connection na ho jaaye tab tak wait kar liye
-async function mongooseConnection() {
-  try {
+async function mongooseConnection(){
+    try {
     console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
     console.log(" MongoDB connection established");
-  } catch (err) { // added (err) here
-    console.error(" MongoDB connection failed:");
-    console.error(err); 
-  }
-async function mongooseConnection(){
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-        console.log("connection established");
     }
-    catch(err){
-        console.log("failed"+err.message);
+    catch (err) { // added (err) here
+        console.error(" MongoDB connection failed:");
+        console.error(err); 
     }
-}
+    async function mongooseConnection(){
+        try{
+            await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            });
+            console.log("connection established");
+        }
+        catch(err){
+            console.log("failed"+err.message);
+        }
+    }
+};
 
 mongooseConnection();
 
